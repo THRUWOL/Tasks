aeb47acf042ea2c4a020602538cf0e963fb496ff
Nikita Yarosh <ThRuWol@yandex.ru>
2020-03-05 16:52:27 +0300

    31

--- Qt_tasks/Task_31/main.cpp
+++ Qt_tasks/Task_31/main.cpp
@@ -36,3 +36,41 @@ string maze[25] = {
 * A = maze[15][0]   D = maze[24][20]
 */
 
+
+void way_check(int y, int x){
+    if (maze[y][x] == ' '){
+        if (maze[y][x+1] == 'A' || maze[y][x+1] == 'B' || maze[y][x+1] == 'C' || maze[y][x+1] == 'D' || maze[y][x+1] == 'E'|| maze[y][x+1] == 'F') cout << maze[y][x+1];
+        else if (maze [y][x+1] == ' '){
+            maze[y][x] = '+';
+            way_check(y, x+1);
+        }
+        if (maze[y][x-1] == 'A' || maze[y][x-1] == 'B' || maze[y][x-1] == 'C' || maze[y][x-1] == 'D' || maze[y][x-1] == 'E'|| maze[y][x-1] == 'F') cout << maze[y][x-1];
+        else if (maze [y][x-1] == ' '){
+            maze[y][x] = '+';
+            way_check(y, x-1);
+        }
+        if (maze[y+1][x] == 'A' || maze[y+1][x] == 'B' || maze[y+1][x] == 'C' || maze[y+1][x] == 'D' || maze[y+1][x] == 'E'|| maze[y+1][x] == 'F') cout << maze[y+1][x];
+        else if (maze [y+1][x] == ' '){
+            maze[y][x] = '+';
+            way_check(y+1, x);
+        }
+        if (maze[y-1][x] == 'A' || maze[y-1][x] == 'B' || maze[y-1][x] == 'C' || maze[y-1][x] == 'D' || maze[y-1][x] == 'E'|| maze[y-1][x] == 'F') cout << maze[y-1][x];
+        else if (maze [y-1][x] == ' '){
+            maze[y][x] = '+';
+            way_check(y-1, x);
+        }
+    }
+    else cout << "Нет выхода.";
+}
+
+int main(){
+    setlocale(LC_ALL, "russian");
+
+    int y, x; //x_max = 24. y_max = 26
+    cin >> x >>y;
+    if (maze[y][x] == '#') cout << "Не верные координаты.";
+    else if (maze[y][x] == 'A' || maze[y][x] == 'B' || maze[y][x] == 'C' || maze[y][x] == 'D' || maze[y][x] == 'E'|| maze[y][x] == 'F') cout << maze[y][x];
+    else if (x <= 24 && y <= 26) way_check(y, x);
+    else cout << "Не верные координаты.";
+
+}
